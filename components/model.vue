053fee31c0b5521c8ef27e6c9aceb15b408d6fd9
ichiineki <a.akari300@gmail.com>
2023-12-10 17:41:43 +0900

    light and rgbe modified

--- components/model.vue
+++ components/model.vue
@@ -2,7 +2,6 @@
     import * as THREE from "three";
     import { OrbitControls } from "three/addons/controls/OrbitControls.js";
     import { GLTFLoader } from 'three/addons/loaders/GLTFLoader.js';
-    import { RGBELoader } from "three/addons/loaders/RGBELoader";
     export default {
 
         mounted() {
